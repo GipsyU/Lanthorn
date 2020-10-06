3a134c711acb4588d1a3d5781a34fb6840527dd5
GipsyU <su_yuheng@foxmail.com>
2020-10-06 11:39:15 +0800

    Lanthorn: Define types

--- arch/x86/include/types.h
+++ arch/x86/include/types.h
@@ -1,3 +1,9 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
+typedef char s8;
+typedef short s16;
+typedef int s32;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
 #endif
