ac26920c1b2a3d006206e36a01a8e590cf062d13
GipsyU <su_yuheng@foxmail.com>
2020-11-12 09:56:20 +0800

    util: Add round_up and round down

---
+++ include/util.h
@@ -0,0 +1,18 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#define ROUND_UP(x, y) ( \
+{ \
+    const typeof(y) _y = (y); \
+    ((x) + _y - 1)) / _y * _y; \
+} \
+)
+
+#define ROUND_DOWN(x, y) ( \
+{ \
+    const typeof(y) _y = (y); \
+    (x) / _y * _y; \
+} \
+)
+
+#endif
