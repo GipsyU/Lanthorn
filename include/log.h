c62b44db8cb904f433cb7e9d37c5e48b3b069383
GipsyU <su_yuheng@foxmail.com>
2020-11-27 17:16:49 +0800

    log: Add color.

--- include/log.h
+++ include/log.h
@@ -3,22 +3,22 @@
 void print(const char *fmt, ...);
 #define info(...)           \
     {                       \
-        print("[INFO] ");   \
+        print("\033[32m[INFO]\033[0m ");   \
         print(__VA_ARGS__); \
     }
 #define debug(...)                                   \
     {                                                \
-        print("[DEBUG] %s:%d ", __FILE__, __LINE__); \
+        print("\033[34m[DEBUG]\033[0m %s:%d ", __FILE__, __LINE__); \
         print(__VA_ARGS__);                          \
     }
 #define warn(...)                                   \
     {                                               \
-        print("[WARN] %s:%d ", __FILE__, __LINE__); \
+        print("\033[33m[WARN]\033[0m %s:%d ", __FILE__, __LINE__); \
         print(__VA_ARGS__);                         \
     }
 #define error(...)                                   \
     {                                                \
-        print("[ERROR] %s:%d ", __FILE__, __LINE__); \
+        print("\033[31m[ERROR]\033[0m %s:%d ", __FILE__, __LINE__); \
         print(__VA_ARGS__);                          \
     }
 #define panic(...)                                   \
