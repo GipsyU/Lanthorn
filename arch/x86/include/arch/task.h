f0e583d069e1974704258f8dcff4b019b440f165
GipsyU <su_yuheng@foxmail.com>
2020-12-05 18:16:12 +0800

    arch: Add arch in include.

--- arch/x86/include/arch/task.h
+++ arch/x86/include/arch/task.h
@@ -2,7 +2,7 @@
 
 #define _X86_TASK_H_
 
-#include <basic.h>
+#include <arch/basic.h>
 
 struct task_t
 {
@@ -15,5 +15,6 @@ int task_init(struct task_t *task, addr_t saddr, size_t ssize, addr_t pre, addr_
 
 void task_switch(struct task_t *o, struct task_t *n);
 
+int task_user_init(struct task_t *task, addr_t ksa, size_t kss, addr_t usa, size_t uss, addr_t pre, addr_t run);
 
 #endif
