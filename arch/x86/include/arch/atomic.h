783ff6521373bbc2c9fcb41a287d958dc0f0bdf4
GipsyU <su_yuheng@foxmail.com>
2020-12-10 10:04:25 +0800

    x86/atomic: Add sub and test.

--- arch/x86/include/arch/atomic.h
+++ arch/x86/include/arch/atomic.h
@@ -3,94 +3,68 @@
 
 #include <arch/basic.h>
 
-#define raw__xchg(x, ptr, size)                          \
-    ({                                                   \
-        __typeof(*(ptr)) __x = (x);                      \
-        switch (size)                                    \
-        {                                                \
-        case 1:                                          \
-        {                                                \
-            volatile u8 *__ptr = (volatile u8 *)(ptr);   \
-            asm volatile("xchgb %0,%1"                   \
-                         : "=q"(__x), "+m"(*__ptr)       \
-                         : "0"(__x)                      \
-                         : "memory");                    \
-            break;                                       \
-        }                                                \
-        case 2:                                          \
-        {                                                \
-            volatile u16 *__ptr = (volatile u16 *)(ptr); \
-            asm volatile("xchgw %0,%1"                   \
-                         : "=r"(__x), "+m"(*__ptr)       \
-                         : "0"(__x)                      \
-                         : "memory");                    \
-            break;                                       \
-        }                                                \
-        case 4:                                          \
-        {                                                \
-            volatile u32 *__ptr = (volatile u32 *)(ptr); \
-            asm volatile("xchgl %0,%1"                   \
-                         : "=r"(__x), "+m"(*__ptr)       \
-                         : "0"(__x)                      \
-                         : "memory");                    \
-            break;                                       \
-        }                                                \
-        }                                                \
-        __x;                                             \
+#define raw__xchg(x, ptr, size)                                                                                        \
+    ({                                                                                                                 \
+        __typeof(*(ptr)) __x = (x);                                                                                    \
+        switch (size)                                                                                                  \
+        {                                                                                                              \
+        case 1: {                                                                                                      \
+            volatile u8 *__ptr = (volatile u8 *)(ptr);                                                                 \
+            asm volatile("xchgb %0,%1" : "=q"(__x), "+m"(*__ptr) : "0"(__x) : "memory");                               \
+            break;                                                                                                     \
+        }                                                                                                              \
+        case 2: {                                                                                                      \
+            volatile u16 *__ptr = (volatile u16 *)(ptr);                                                               \
+            asm volatile("xchgw %0,%1" : "=r"(__x), "+m"(*__ptr) : "0"(__x) : "memory");                               \
+            break;                                                                                                     \
+        }                                                                                                              \
+        case 4: {                                                                                                      \
+            volatile u32 *__ptr = (volatile u32 *)(ptr);                                                               \
+            asm volatile("xchgl %0,%1" : "=r"(__x), "+m"(*__ptr) : "0"(__x) : "memory");                               \
+            break;                                                                                                     \
+        }                                                                                                              \
+        }                                                                                                              \
+        __x;                                                                                                           \
     })
 
-#define xchg(ptr, v) \
-    raw__xchg((v), (ptr), sizeof(*ptr))
+#define xchg(ptr, v) raw__xchg((v), (ptr), sizeof(*ptr))
 
-#define raw_cmpxchg(ptr, old, new, size)                 \
-    ({                                                   \
-        __typeof__(*(ptr)) __ret;                        \
-        __typeof__(*(ptr)) __old = (old);                \
-        __typeof__(*(ptr)) __new = (new);                \
-        switch (size)                                    \
-        {                                                \
-        case 1:                                          \
-        {                                                \
-            volatile u8 *__ptr = (volatile u8 *)(ptr);   \
-            asm volatile("cmpxchgb %2,%1"                \
-                         : "=a"(__ret), "+m"(*__ptr)     \
-                         : "q"(__new), "0"(__old)        \
-                         : "memory");                    \
-            break;                                       \
-        }                                                \
-        case 2:                                          \
-        {                                                \
-            volatile u16 *__ptr = (volatile u16 *)(ptr); \
-            asm volatile("cmpxchgw %2,%1"                \
-                         : "=a"(__ret), "+m"(*__ptr)     \
-                         : "r"(__new), "0"(__old)        \
-                         : "memory");                    \
-            break;                                       \
-        }                                                \
-        case 4:                                          \
-        {                                                \
-            volatile u32 *__ptr = (volatile u32 *)(ptr); \
-            asm volatile("cmpxchgl %2,%1"                \
-                         : "=a"(__ret), "+m"(*__ptr)     \
-                         : "r"(__new), "0"(__old)        \
-                         : "memory");                    \
-            break;                                       \
-        }                                                \
-        }                                                \
-        __ret;                                           \
+#define raw_cmpxchg(ptr, old, new, size)                                                                               \
+    ({                                                                                                                 \
+        __typeof__(*(ptr)) __ret;                                                                                      \
+        __typeof__(*(ptr)) __old = (old);                                                                              \
+        __typeof__(*(ptr)) __new = (new);                                                                              \
+        switch (size)                                                                                                  \
+        {                                                                                                              \
+        case 1: {                                                                                                      \
+            volatile u8 *__ptr = (volatile u8 *)(ptr);                                                                 \
+            asm volatile("cmpxchgb %2,%1" : "=a"(__ret), "+m"(*__ptr) : "q"(__new), "0"(__old) : "memory");            \
+            break;                                                                                                     \
+        }                                                                                                              \
+        case 2: {                                                                                                      \
+            volatile u16 *__ptr = (volatile u16 *)(ptr);                                                               \
+            asm volatile("cmpxchgw %2,%1" : "=a"(__ret), "+m"(*__ptr) : "r"(__new), "0"(__old) : "memory");            \
+            break;                                                                                                     \
+        }                                                                                                              \
+        case 4: {                                                                                                      \
+            volatile u32 *__ptr = (volatile u32 *)(ptr);                                                               \
+            asm volatile("cmpxchgl %2,%1" : "=a"(__ret), "+m"(*__ptr) : "r"(__new), "0"(__old) : "memory");            \
+            break;                                                                                                     \
+        }                                                                                                              \
+        }                                                                                                              \
+        __ret;                                                                                                         \
     })
 
-#define cmpxchg(ptr, old, new) \
-    raw_cmpxchg((ptr), (old), (new), sizeof(*ptr))
+#define cmpxchg(ptr, old, new) raw_cmpxchg((ptr), (old), (new), sizeof(*ptr))
 
 struct atomic_t
 {
     int counter;
 };
 
-#define ATOMIC_INIT(i) \
-    {                  \
-        (i)            \
+#define ATOMIC_INIT(i)                                                                                                 \
+    {                                                                                                                  \
+        (i)                                                                                                            \
     }
 
 static inline int atomic_read(const struct atomic_t *v)
@@ -103,18 +77,28 @@ static inline void atomic_set(struct atomic_t *v, int i)
     v->counter = i;
 }
 
-static inline void atomic_add(int i, struct atomic_t *v)
+static inline void atomic_add(struct atomic_t *v, int i)
 {
-    asm volatile("addl %1,%0"
-                 : "+m"(v->counter)
-                 : "ir"(i));
+    asm volatile("addl %1,%0" : "+m"(v->counter) : "ir"(i));
 }
 
-static inline void atomic_sub(int i, struct atomic_t *v)
+static inline void atomic_sub(struct atomic_t *v, int i)
 {
-    asm volatile("subl %1,%0"
-                 : "+m"(v->counter)
-                 : "ir"(i));
+    asm volatile("subl %1,%0" : "+m"(v->counter) : "ir"(i));
+}
+
+/**
+ * Atomically subtracts @i from @v and returns
+ * true if the result is zero, or false for all
+ * other cases.
+ */
+static inline int atomic_sub_and_test(struct atomic_t *v, int i)
+{
+    unsigned char c;
+
+    asm volatile("subl %2,%0; sete %1" : "+m"(v->counter), "=qm"(c) : "ir"(i) : "memory");
+    
+    return c;
 }
 
 static inline int atomic_cmpxchg(struct atomic_t *v, int old, int new)
