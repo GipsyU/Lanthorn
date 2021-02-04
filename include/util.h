5fbf542ff8a2afdf8a840c82cd990c29578daefe
GipsyU <su_yuheng@foxmail.com>
2021-02-04 14:00:59 +0800

    log: Mov va macro to util.

--- include/util.h
+++ include/util.h
@@ -32,4 +32,12 @@
 
 #define min(a, b) ({ ((a) < (b) ? (a) : (b)); })
 
+#define _INTSIZEOF(n) ((sizeof(n) + sizeof(int) - 1) & ~(sizeof(int) - 1))
+
+#define va_start(ap, v) (ap = (char *)&v + _INTSIZEOF(v))
+
+#define va_arg(ap, t) (*(t *)((ap += _INTSIZEOF(t)) - _INTSIZEOF(t)))
+
+#define va_end(ap) (ap = (char *)0)
+
 #endif
